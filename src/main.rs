ef563624eca5a63488388ae4abd5da9682b73804
cybaek <vovoboss@gmail.com>
2021-08-12 00:10:41 +0900

    list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json;
-use sled::{self, Config, Db, Result};
+use sled::{self, Db, Result};
 use std::str;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,6 +22,18 @@ impl TodoList {
             lastest_id: format!("0"),
         }
     }
+
+    fn list(&self) -> Vec<Todo> {
+        let mut list = Vec::new();
+        for v in self.db.iter() {
+            println!("{:?}", str::from_utf8(&v.as_ref().unwrap().0));
+            println!("{:?}", str::from_utf8(&v.as_ref().unwrap().1));
+            let stodo = str::from_utf8(&v.as_ref().unwrap().1).unwrap();
+            let todo: Todo = serde_json::from_str(stodo).unwrap();
+            list.push(todo);
+        }
+        list
+    }
     fn add(&mut self, title: String) -> Result<()> {
         let id = format!("{}", self.lastest_id.parse::<usize>().unwrap() + 1);
         self.lastest_id = id.clone();
@@ -33,6 +45,10 @@ impl TodoList {
         let r = serde_json::to_string(&todo).unwrap();
         self.db.insert(id.as_bytes(), r.as_bytes())?;
         Ok(())
+    }
+
+    fn remove(&mut self, id: String) {
+        self.db.remove(id.as_bytes()).unwrap();
     }
 
     fn toggle_done(&mut self, id: String) {
@@ -72,11 +88,13 @@ async fn main() -> Result<()> {
     //todo_list.add(format!("ok"))?;
     //todo_list.add(format!("thisis good"))?;
     let todo = todo_list.get(format!("1"));
-    println!("{:?}", todo);
-    let todo = todo_list.get(format!("2"));
-    println!("{:?}", todo);
-    todo_list.toggle_done(format!("2"));
-    let todo = todo_list.get(format!("2"));
-    println!("{:?}", todo);
+    // println!("{:?}", todo);
+    // let todo = todo_list.get(format!("2"));
+    // println!("{:?}", todo);
+    // todo_list.toggle_done(format!("2"));
+    // let todo = todo_list.get(format!("2"));
+    // println!("{:?}", todo);
+    let list = todo_list.list();
+    println!("{:?}", list);
     Ok(())
 }
